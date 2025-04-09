a4e71ca87ac8fa1f37deaa682b31b7712ed7f415
Tukue Gebregergis <tukue.geb@gmail.com>
2025-04-09 17:49:35 +0200

    adding timestamp for response

--- tasks/views.py
+++ tasks/views.py
@@ -3,18 +3,23 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
+from datetime import datetime
 
 class TaskAPIView(APIView):
     def get(self, request):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+        response = Response(serializer.data)
+        response['X-Timestamp'] = datetime.utcnow().isoformat()  # Add timestamp header
+        return response
 
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            response['X-Timestamp'] = datetime.utcnow().isoformat()  # Add timestamp header
+            return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk):
