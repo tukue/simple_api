01193095516967d0ca8804142e131521ec9be683
Tukue Gebregergis <tukue.geb@gmail.com>
2025-04-09 19:17:40 +0200

    reading secret key from env and adding .env to gitignore

--- simple_api/settings.py
+++ simple_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=f7&0rlj)y*&!hj5^l8q3guw9gc(&7!ze^1xb1l_70xd$=uked'
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='fallback-secret-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
