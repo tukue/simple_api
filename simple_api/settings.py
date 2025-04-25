83c5fc7c69e3bf016b191b93d57f9a1a3987b69f
Tukue Gebregergis <tukue.geb@gmail.com>
2025-04-25 14:56:11 +0200

    limiting the number of api request per user

--- simple_api/settings.py
+++ simple_api/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '100/day',
+    },
+}
